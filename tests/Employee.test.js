1d2f1413484f4c893f9da3f66a142264211bef6c
BrownCAB <110314270+BrownCAB@users.noreply.github.com>
2022-10-21 11:25:07 -0700

    Setting up Employee Test for testing

---
+++ tests/Employee.test.js
@@ -0,0 +1,10 @@
+const Employee = require('../lib/Employee.js')
+
+it ('Should create an instance of class Employee', () => {
+   const newEmployee = new Employee employee() // arange, act, and assert
+   expect(typeOf newEmployee.toBe('object')
+})
+
+// test or it can be used as statement keyword
+
+// node i jest
